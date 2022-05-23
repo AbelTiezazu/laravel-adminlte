f37695f70214ae7a5e8a7f4f2d944eaa8215d22d
AbelTiezazu <44517705+AbelTiezazu@users.noreply.github.com>
2022-05-23 12:18:43 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <h1 class="text-black-50">You are logged in!</h1>
+
+
+        <div class="position-relative p-3 bg-gray" style="height: 180px">
+        <div class="ribbon-wrapper ribbon-xl">
+  <div class="ribbon bg-danger text-xl">
+    Abel
+  </div>
+</div>
+
+
+    </div>
+
+    <div class="info-box">
+  <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+  <div class="info-box-content">
+    <span class="info-box-text">Messages</span>
+    <span class="info-box-number">1,410</span>
+  </div>
+</div>
+
+   
+    </div>
+@endsection
